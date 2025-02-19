0b27cced9a745b26b8c9a458307a571b9cefeb3d
zafarzhon <zafarzhon77@gmail.com>
2025-02-19 12:56:47 +0600

    changed image size and added js logic for requesting datas

--- assets/js/send_message.js
+++ assets/js/send_message.js
@@ -1,5 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
+  let success_message = document.getElementById("success_message");
+  let checkbox_radio = document.querySelector('input[type="checkbox"]');
+  let submit_button = document.querySelector("button[type='submit']");
+
   const form = document.querySelector(".request__form-body");
+
+  if (checkbox_radio && submit_button) {
+    let checkbox_value = checkbox_radio.checked;
+    submit_button.disabled = !checkbox_value;
+
+    checkbox_radio.addEventListener("change", function () {
+      submit_button.disabled = !this.checked;
+    });
+  }
 
   form.addEventListener("submit", function (event) {
     event.preventDefault();
@@ -12,6 +25,36 @@ document.addEventListener("DOMContentLoaded", function () {
     };
 
     console.log(dataFromMainPage);
+
+    fetch("https://bektransgroup.com/backend/cpanel_mail.php", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dataFromMainPage),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (success_message) {
+          success_message.innerHTML = data.status;
+          success_message.className = "n-success";
+          setTimeout(() => {
+            success_message.innerHTML = null;
+            success_message.className = "";
+          }, 5000);
+        } else {
+          console.log(data.status);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
     form.reset();
+
+    if (submit_button) {
+      submit_button.disabled = true;
+    }
   });
 });
--- assets/js/send_message2.js
+++ assets/js/send_message2.js
@@ -1,5 +1,45 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   const contactsForm = document.getElementById("contactForm");
+//   contactsForm.addEventListener("submit", function (event) {
+//     event.preventDefault();
+
+//     const dataFromContactsPage = {
+//       name: contactsForm.querySelector('input[type="text"]').value,
+//       email: contactsForm.querySelector('input[type="email"]').value,
+//       phone: contactsForm.querySelector('input[placeholder="Phone"]').value,
+//       checkbox: contactsForm.querySelector('input[type="checkbox"]').checked,
+//     };
+
+//     fetch("https://bektransgroup.com/backend/cpanel_mail.php", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(dataFromContactsPage),
+//     })
+//       .then((response) => response.json())
+//       .then((data) => console.log(data))
+//       .catch((error) => console.error(error));
+//     contactsForm.reset();
+//   });
+// });
+
 document.addEventListener("DOMContentLoaded", function () {
+  let success_message = document.getElementById("success_message");
+  let checkbox_radio = document.querySelector('input[type="checkbox"]');
+  let submit_button = document.querySelector("button[type='submit']");
+
   const contactsForm = document.getElementById("contactForm");
+
+  if (checkbox_radio && submit_button) {
+    let checkbox_value = checkbox_radio.checked;
+    submit_button.disabled = !checkbox_value;
+
+    checkbox_radio.addEventListener("change", function () {
+      submit_button.disabled = !this.checked;
+    });
+  }
+
   contactsForm.addEventListener("submit", function (event) {
     event.preventDefault();
 
@@ -11,6 +51,36 @@ document.addEventListener("DOMContentLoaded", function () {
     };
 
     console.log(dataFromContactsPage);
+
+    fetch("https://bektransgroup.com/backend/cpanel_mail.php", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dataFromContactsPage),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (success_message) {
+          success_message.innerHTML = data.status;
+          success_message.className = "n-success";
+          setTimeout(() => {
+            success_message.innerHTML = null;
+            success_message.className = "";
+          }, 5000);
+        } else {
+          console.log(data.status);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
     contactsForm.reset();
+
+    if (submit_button) {
+      submit_button.disabled = true;
+    }
   });
 });
